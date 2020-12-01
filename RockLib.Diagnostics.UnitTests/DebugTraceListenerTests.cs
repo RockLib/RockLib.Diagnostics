aa20829ccdddd006e68e04a1c1f23ecb0f27063e
Brian Friesen <brianfriesen@quickenloans.com>
2020-11-30 22:04:16 -0500

    Add DebugTraceListener

---
+++ RockLib.Diagnostics.UnitTests/DebugTraceListenerTests.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace RockLib.Diagnostics.UnitTests
+{
+    public class DebugTraceListenerTests
+    {
+        [Fact(DisplayName = "Constructor sets default name")]
+        public void ConstructorHappyPath1()
+        {
+            var traceListener = new DebugTraceListener();
+
+            traceListener.Name.Should().Be("");
+        }
+
+        [Fact(DisplayName = "Constructor sets specified name")]
+        public void ConstructorHappyPath2()
+        {
+            var traceListener = new DebugTraceListener("TestName");
+
+            traceListener.Name.Should().Be("TestName");
+        }
+    }
+}
---
+++ RockLib.Diagnostics/DebugTraceListener.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace RockLib.Diagnostics
+{
+    /// <summary>
+    /// A <see cref="TraceListener"/> that outputs to debug.
+    /// </summary>
+    public class DebugTraceListener : TraceListener
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebugTraceListener"/> class.
+        /// </summary>
+        /// <param name="name">The name of the <see cref="DebugTraceListener"/>.</param>
+        public DebugTraceListener(string name = null)
+            : base(name)
+        {
+        }
+
+        /// <summary>
+        /// Writes the specified message to debug.
+        /// </summary>
+        /// <param name="message">A message to write.</param>
+        public override void Write(string message) =>
+            Debug.Write(message);
+
+        /// <summary>
+        /// Writes the specified message to debug, followed by a line terminator.
+        /// </summary>
+        /// <param name="message">A message to write.</param>
+        public override void WriteLine(string message) =>
+            Debug.WriteLine(message);
+    }
+}
